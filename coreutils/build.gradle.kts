0f74f4aa0726f752c082d2f0275358c4ec4ec995
gowthambharathn <barathingo28@gmail.com>
2026-02-09 08:21:36 +0530

    adding jitpack publish

--- coreutils/build.gradle.kts
+++ coreutils/build.gradle.kts
@@ -1,17 +1,16 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
     namespace = "infinity.developers.coreutils"
-    compileSdk {
-        version = release(36)
-    }
+
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 26
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -33,10 +32,12 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
@@ -46,6 +47,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -54,4 +56,17 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.2.0")
     implementation("androidx.compose.animation:animation:1.6.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.1")
+}
+
+afterEvaluate {
+    extensions.configure<org.gradle.api.publish.PublishingExtension>("publishing") {
+        publications {
+            create<org.gradle.api.publish.maven.MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.gowthambharathn"
+                artifactId = "coreutils"
+                version = "3.3"
+            }
+        }
+    }
 }
